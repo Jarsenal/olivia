b7bd6d539f6a9ac2e2cf79d5316126e0b5485068
Jarsenal <43666111+Jarsenal@users.noreply.github.com>
2021-10-10 22:30:06 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! This is Olivia Z"
+    return "<html><P style='font-size:8px;text-align:center'>If you can read me you are too close to the screen, back off!</P></html>"
