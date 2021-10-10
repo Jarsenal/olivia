c96b2ff271497a195a7cf96eea3dc6e4b0d9e54d
Jarsenal <43666111+Jarsenal@users.noreply.github.com>
2021-10-10 22:32:46 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><P style='font-size:8px;text-align:center'>If you can read me you are too close to the screen, back off!</P></html>"
+    return "<html><P style='font-size:7px;text-align:center'>If you can read me you are too close to the screen!</P></html>"
